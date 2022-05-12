51ce2516e20f3fb1ee1ff184459a02e6ebcb2559
iyj6707 <iyj6707@naver.com>
2022-05-12 20:44:18 +0900

    feat: add generic view

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pybo'
+# urlpatterns = [
+#     path('', views.index),
+#     path('<int:question_id>/', views.detail),
+# ]
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.IndexView.as_view()),
+    path('<int:pk>/', views.DetailView.as_view()),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
-def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신 걸 환영합니다.")
+from .models import Question
+
+
+# def index(request):
+#     question_list = Question.objects.order_by('-create_date')
+#     context = {'question_list': question_list}
+#     return render(request, 'pybo/question_list.html', context)
+#
+#
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     context = {'question': question}
+#     return render(request, 'pybo/question_detail.html', context)
+
+class IndexView(generic.ListView):
+    def get_queryset(self):
+        return Question.objects.order_by('-create_date')
+
+
+class DetailView(generic.DetailView):
+    model = Question
