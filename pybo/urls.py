d6d11d925896f83d7afcefdf1b6614747bdeaf80
iyj6707 <iyj6707@naver.com>
2022-05-12 20:47:58 +0900

    feat: add url alias

--- pybo/urls.py
+++ pybo/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from . import views
 
 app_name = 'pybo'
-# urlpatterns = [
-#     path('', views.index),
-#     path('<int:question_id>/', views.detail),
-# ]
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
-    path('<int:pk>/', views.DetailView.as_view()),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
+
+# for generic view
+# urlpatterns = [
+#     path('', views.IndexView.as_view()),
+#     path('<int:pk>/', views.DetailView.as_view()),
+# ]
--- pybo/views.py
+++ pybo/views.py
@@ -4,21 +4,22 @@ from django.views import generic
 from .models import Question
 
 
-# def index(request):
-#     question_list = Question.objects.order_by('-create_date')
-#     context = {'question_list': question_list}
-#     return render(request, 'pybo/question_list.html', context)
+def index(request):
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+# for generic view
+# class IndexView(generic.ListView):
+#     def get_queryset(self):
+#         return Question.objects.order_by('-create_date')
 #
 #
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     context = {'question': question}
-#     return render(request, 'pybo/question_detail.html', context)
-
-class IndexView(generic.ListView):
-    def get_queryset(self):
-        return Question.objects.order_by('-create_date')
-
-
-class DetailView(generic.DetailView):
-    model = Question
+# class DetailView(generic.DetailView):
+#     model = Question
