dcc0fae12eb2a1e1c64ced9a948616179ef1b3eb
iyj6707 <iyj6707@naver.com>
2022-06-22 16:35:48 +0900

    feat: add paging feature

--- pybo/views.py
+++ pybo/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseNotAllowed
 from django.utils import timezone
@@ -7,8 +8,11 @@ from .forms import QuestionForm, AnswerForm
 
 
 def index(request):
+    page = request.GET.get('page', '1')
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 
